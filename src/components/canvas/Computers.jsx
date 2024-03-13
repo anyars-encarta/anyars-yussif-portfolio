b41c81d93342efad16c6eeb0c6a80e8438a3014f
anyars-encarta <anyarsencarta@gmail.com>
2024-03-13 14:59:58 +0000

    Working on Hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -17,7 +17,7 @@ const Computers = () => {
 
 const ComputersCanvas = () => {
   return (
-    <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameLoop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
         <Computers />
