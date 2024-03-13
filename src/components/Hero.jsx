0ed55e2b9fa332e682306d7c48d3867aaacf0041
anyars-encarta <anyarsencarta@gmail.com>
2024-03-13 14:17:15 +0000

    Correct Hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full mt-5 bg-[#915eff]' />
-          <div className='w-1 sm:h-80 h-40 ml-2 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div >
