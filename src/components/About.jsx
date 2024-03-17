2b41ff162005db5c03136a2208aa800d4ef3305f
anyars-encarta <anyarsencarta@gmail.com>
2024-03-17 15:58:31 +0000

    Implement experience section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
       <div className='mt-20 flex flex-wrap gap-10'>
         {
           services.map((service, index) => (
-            <ServiceCard key={services.title} index={index} {...service} />
+            <ServiceCard key={service.title} index={index} {...service} />
           ))
         }
       </div>
