87b9b1702c0bfd7d5d20351be034ae0df9fd87ea
anyars-encarta <anyarsencarta@gmail.com>
2024-03-19 07:43:18 +0000

    Add moving stars

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -31,6 +31,8 @@ const EarthCanvas = () => {
         <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Earth />
       </Suspense>
+
+      <Preload all />
     </Canvas>
   )
 };
