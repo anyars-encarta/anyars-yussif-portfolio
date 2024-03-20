ca6b211f5092c6059a266f27663157f36e3cd166
anyars-encarta <anyarsencarta@gmail.com>
2024-03-20 08:02:14 +0000

    Correct experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -210,6 +210,7 @@ const experiences = [
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
+      "Mentoring junior developers, Peer programming, Project based learning, Collaboration.",
     ],
   },
 ];
