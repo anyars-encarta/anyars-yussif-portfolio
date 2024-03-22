e27ddecd4206024b216bbe317fc63f190c85581d
anyars-encarta <anyarsencarta@gmail.com>
2024-03-22 03:06:03 +0000

    Reduce tech stacks

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,10 +135,10 @@ const technologies = [
     name: "HTML 5",
     icon: html,
   },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
+  // {
+  //   name: "CSS 3",
+  //   icon: css,
+  // },
   {
     name: "JavaScript",
     icon: javascript,
@@ -151,38 +151,38 @@ const technologies = [
     name: "React JS",
     icon: reactjs,
   },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
   {
     name: "Tailwind CSS",
     icon: tailwind,
   },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
-    name: "git",
-    icon: git,
-  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
+  // {
+  //   name: "git",
+  //   icon: git,
+  // },
   {
     name: "PostgreSQL",
     icon: postgresql,
   },
-  {
-    name: "Ruby on Rails",
-    icon: rails,
-  },
+  // {
+  //   name: "Ruby on Rails",
+  //   icon: rails,
+  // },
   // {
   //   name: "MySQL",
   //   icon: mysql,
